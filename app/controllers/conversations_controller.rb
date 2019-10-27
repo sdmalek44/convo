f1e384b79bdcf5504640e35c5b6c5d4915822f4d
sdmalek44 <malek.stephen44@gmail.com>
2019-10-27 16:05:01 -0600

    Add create action to conversations controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,4 +2,21 @@ class ConversationsController < ApplicationController
   def index
     render json: Conversation.all
   end
+
+  def create
+    conversation = Conversation.new(conversation_params)
+
+    if conversation.save
+      serialized_convo = ConversationSerializer.new(conversation)
+      serialized_data = ActiveModelSerializers::Adapter::Json.new(serialized_convo).serializable_hash
+      ActionCable.server.broadcast 'conversations_channel', serialized_data
+      head :ok
+    end
+  end
+
+  private
+
+  def conversation_params
+    params.require(:conversation).permit(:title)
+  end
 end
